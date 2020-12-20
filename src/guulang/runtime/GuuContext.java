1d4ded585a8c6fe0f0991c66dd52f984bf295034
Artem Kobzar <dendishiz0@gmail.com>
2020-12-20 12:28:14 +0200

    chore: change materialized frame on virtual frame.

--- src/guulang/runtime/GuuContext.java
+++ src/guulang/runtime/GuuContext.java
@@ -1,15 +1,16 @@
 package guulang.runtime;
 
 import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.frame.FrameDescriptor;
 import guulang.GuuLang;
 
 public final class GuuContext {
     private final GuuLang language;
     private final GuuProcedureRegistry procedureRegistry;
-    private final MaterializedFrame globalVariablesFrame = Truffle.getRuntime()
-            .createMaterializedFrame(new Object[0]);
+    private final VirtualFrame globalVariablesFrame = Truffle.getRuntime()
+            .createVirtualFrame(new Object[0], new FrameDescriptor());
 
     public GuuContext(GuuLang language, GuuProcedureRegistry procedureRegistry) {
         this.language = language;
